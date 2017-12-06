974a6e92107ff1ee88adaa8b50b0f5f7ecacba2f
Hardik Kothari <hardik.kothari@xoriant.com>
2017-12-06 16:28:24 +0530

    Libraries added

--- AppCI/AppCI/MainActivity.cs
+++ AppCI/AppCI/MainActivity.cs
@@ -1,6 +1,9 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace AppCI
 {
@@ -10,6 +13,8 @@ namespace AppCI
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+            AppCenter.Start("db5620c0-9835-465e-b04f-53db08dd75bf",
+                   typeof(Analytics), typeof(Crashes));
 
             // Set our view from the "main" layout resource
             // SetContentView (Resource.Layout.Main);
