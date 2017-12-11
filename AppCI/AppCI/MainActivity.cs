04d97487b60eca1a4f50ba454bf2745edc0f6c3c
Hardik Kothari <hardik.kothari@xoriant.com>
2017-12-11 16:56:28 +0530

    App Updated

--- AppCI/AppCI/MainActivity.cs
+++ AppCI/AppCI/MainActivity.cs
@@ -1,24 +1,30 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
+using AppCI;
 
-namespace AppCI
+namespace TestApp
 {
-    [Activity(Label = "AppCI", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
-            AppCenter.Start("db5620c0-9835-465e-b04f-53db08dd75bf",
-                   typeof(Analytics), typeof(Crashes));
+	[Activity(Label = "TestApp", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
-            SetContentView (Resource.Layout.Main);
-        }
-    }
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			EditText txtUsername = FindViewById<EditText>(Resource.Id.username);
+			EditText txtPassword = FindViewById<EditText>(Resource.Id.password);
+
+			Button button = FindViewById<Button>(Resource.Id.loginBtn);
+
+			button.Click += delegate {StartActivity(typeof(HomeScreen));};
+		}
+	}
 }
+
 
