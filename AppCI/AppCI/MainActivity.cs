74b83926bf6d898978b44c6c1204ceca38d5fee3
Hardik Kothari <hardik.kothari@xoriant.com>
2017-12-11 17:21:23 +0530

    Crash resolved

--- AppCI/AppCI/MainActivity.cs
+++ AppCI/AppCI/MainActivity.cs
@@ -2,6 +2,9 @@
 using Android.Widget;
 using Android.OS;
 using AppCI;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace TestApp
 {
@@ -12,8 +15,12 @@ namespace TestApp
 		{
 			base.OnCreate(savedInstanceState);
 
-			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
+            // Analytics for crashes
+            AppCenter.Start("10c5925e-882d-4fff-9e6b-1b418ac77257",
+                   typeof(Analytics), typeof(Crashes));
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.login);
 
 			// Get our button from the layout resource,
 			// and attach an event to it
@@ -22,7 +29,17 @@ namespace TestApp
 
 			Button button = FindViewById<Button>(Resource.Id.loginBtn);
 
-			button.Click += delegate {StartActivity(typeof(HomeScreen));};
+			button.Click += delegate 
+            {
+                if (txtUsername.Text.Equals("admin") && txtPassword.Text.Equals("admin"))
+                {
+                    StartActivity(typeof(HomeScreen));
+                }
+                else
+                {
+                    Toast.MakeText(this, "Invalid username or password.", ToastLength.Short).Show();
+                }
+            };
 		}
 	}
 }
